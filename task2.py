96ba145f447cbb88afff241a3323250be73c19e3
Nikita <PerevozchikovNikita2007@yandex.ru>
2024-10-14 19:34:35 +0300

    тестовое изменение

--- task2.py
+++ task2.py
@@ -1,31 +1,5 @@
 
-'''
-print("Введите число:")
-n = int(input())
-r = 0
-while n > 0:
-    ostatok = n%10
-    r = r*10 + ostatok
-    n = n // 10
-print(r)
 
-
-
-print("Ведите число:")
-i = input()
-g = int(i)
-t = 0
-sum = 0
-while g > 0:
-    h = g % 10
-    g = g // 10
-    t = t + 1
-
-    sum = h * h * h + sum
-
-print(t)
-print(sum)
-'''
 
 
 
