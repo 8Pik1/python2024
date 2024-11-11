de54a65b1ad83ad2ff16459aedeff0b10cdf4524
Nikita <PerevozchikovNikita2007@yandex.ru>
2024-11-11 23:39:49 +0300

    задание к 2024-09-29

---
+++ 2024-09-29/task.py
@@ -0,0 +1,35 @@
+print("введите первое число:")
+a = int(input())
+print("введите второе число:")
+b = int(input())
+print("введите второе число:")
+c = int(input())
+print("сумма чисел:"+str(a+b+c))
+print("произведение чисел:"+str(a*b*c))
+
+
+print("введите вашу зарплату")
+i = int(input())
+print("введите сумму месячного платежа по кредиту")
+h = int(input())
+print("введите вашу задолженность за коммунальные услуги")
+k = int(input())
+print("ваш остаток:", str(i-h-k))
+
+
+print("введите первую диагональ ромба")
+d1 = int(input())
+print("введите вторую диагональ ромба")
+d2 = int(input())
+print("площадь ромба = "+ str(d1*d2/2))
+
+
+print("To be\n"
+      "or not\n"
+      "to be")
+
+
+print("Life is what happens\n"
+      "\twhen\n"
+      "\t\tyou re busy making other plans\n"
+      "\t\t\t\t\t\t\t\t\tjohn Lennon")
