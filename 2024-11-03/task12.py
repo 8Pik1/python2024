15e22e41c88f117ce510d4f5f92748a7ad8324f3
Nikita <PerevozchikovNikita2007@yandex.ru>
2024-11-13 20:33:28 +0300

    задание к 2024-11-03

--- 2024-11-03/task12.py
+++ 2024-11-03/task12.py
@@ -11,9 +11,11 @@ hp1 = 100
 hp2 = 110
 damage = 10
 
-
 print("Идя по тропинке вы видите дракона:")
-print("Он атакует вас первым")
+first = int(input("Кто атакует первым? (0 - игрок, 1 - дракон): "))
+if first == 1:
+    hp1 = hp1 - damage
+    print("Дракон нанес урон:", damage)
 game_over = False
 while not game_over:
     print("---------------------------------------------")
@@ -30,7 +32,7 @@ while not game_over:
     if action == 1:
         hp2 = hp2 - damage
         print(descriptions[1], damage)
-        if hp1 >= 30 and hp1 < 50:
+        if (hp1 >= 30 and hp1 < 50) or first == 1:
             hp2 = hp2 - damage
             print(descriptions[1], damage)
 
@@ -39,6 +41,7 @@ while not game_over:
         print("> Игра окончина, вы победили")
     else:
         hp1 = hp1 - damage
+        print("Дракон нанес урон:", damage)
     if hp1 <= 0:
         game_over = True
         print("> Игра окончина, вы проиграли")
