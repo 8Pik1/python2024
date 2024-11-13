a927386ad8269db2db92240bd74ae2305c75b30a
Nikita <PerevozchikovNikita2007@yandex.ru>
2024-11-13 20:13:01 +0300

    задание к 2024-11-03

--- 2024-11-03/task12.py
+++ 2024-11-03/task12.py
@@ -3,50 +3,42 @@ attack = [
     "Атаковать магией (1)",
 ]
 descriptions = [
-    "Нанесли урон мечем",
-    "Нанесли урон магией",
+    "Нанесли урон мечем: ",
+    "Нанесли урон магией: ",
 ]
 
 hp1 = 100
-attack1 = 0
-hp2 = 150
-attack2 = 20
+hp2 = 110
+damage = 10
+
 
 print("Идя по тропинке вы видите дракона:")
 print("Он атакует вас первым")
 game_over = False
 while not game_over:
     print("---------------------------------------------")
-    print("Ваше здоровье:",hp1 - attack2)
+    print("Ваше здоровье:",hp1)
     print("Здоровье дракона:", hp2)
-    attack = input("Как будем атакавать? - (0 - 1): ")
 
-    if attack == 0:
-        print(descriptions[0])
-        if hp1 < 50:
-            print(descriptions[0])
-    if attack == 1 and hp1 > 30:
-        print(descriptions[1])
-        if hp1 < 30:
-            print(descriptions[1])
+    action = int(input("Как будем атакавать? - (0 - 1): "))
+    if action == 0:
+        hp2 = hp2 - damage
+        print(descriptions[0], damage)
+        if hp1 >= 50:
+            hp2 = hp2 - damage
+            print(descriptions[0], damage)
+    if action == 1:
+        hp2 = hp2 - damage
+        print(descriptions[1], damage)
+        if hp1 >= 30 and hp1 < 50:
+            hp2 = hp2 - damage
+            print(descriptions[1], damage)
 
-        if attack == 0:
-            hp1 = hp1 - attack2
-            hp2 = hp2 - 15
-            print(descriptions[1])
-            if hp1 < 50:
-                hp1 = hp1 - attack2
-                hp2 = hp2 - 10
-                print(descriptions[1])
-        if attack == 1 and hp1 > 30:
-            hp1 = hp1 - attack2
-            hp2 = hp2 - 20
-            print(descriptions[1])
-            if hp1 < 30:
-                hp1 = hp1 - attack2
-                hp2 = hp2 - 10
-                print(descriptions[1])
-        if hp1 == 0:
-            game_over = True
-        if hp2 == 0:
-            game_over = True
+    if hp2 <= 0:
+        game_over = True
+        print("> Игра окончина, вы победили")
+    else:
+        hp1 = hp1 - damage
+    if hp1 <= 0:
+        game_over = True
+        print("> Игра окончина, вы проиграли")
